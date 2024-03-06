301f266a4e3b732668cf6c402cb2038d83137ada
mirzakarabulut <mirzakara1234@gmail.com>
2024-03-06 14:13:58 +0300

    added new function

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
       console.log(data3);
       fs.writeFile('./starter/txt/final.txt',`${data2}\n${data3}`, 'utf-8', (err) => {
         console.log("Your filw has been written");
+        console.log("srfsg");
   })
   })
   })
