40606d674944ad4073f1f8ad4ecbe6ba62cb8b6a
mirzakarabulut <mirzakara1234@gmail.com>
2024-03-09 17:49:54 +0300

    I added my own modules and some public  modules

--- index.js
+++ index.js
@@ -1,25 +1,55 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./starter/modules/replaceTemplate');
+/////////////////////////
+// SERVER
 
-// Blocking-Sync
-// const textIn = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
-// console.log(textIn);
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8');
+const tempCards = fs.readFileSync(`${__dirname}/starter/templates/template-cards.html`, 'utf-8');
 
-// const textOut = `This is what we know about avacado: ${textIn}.\nCreated in ${Date.now()}`;
-// fs.writeFileSync('./starter/txt/output.txt', textOut);
-// console.log("New file is written");
 
-// NoBlocking-Async
+const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`);
+const readJSON = JSON.parse(data);
+const slugs = readJSON.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-//   fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//     console.log(data2);
-//     fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data3) => {
-//       console.log(data3);
-//       fs.writeFile('./starter/txt/final.txt',`${data2}\n${data3}`, 'utf-8', (err) => {
-//         console.log("Your filw has been written");
-//   })
-//   })
-//   })
-// });
-// console.log("Will read file");
+  
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // OVERVIEW PAGE
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    
+    const cardsHTML = readJSON.map(el => replaceTemplate(tempCards, el)).join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+    res.end(output);
+
+  // PRODUCT PAGE
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = readJSON[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+  // API PAGE  
+  } else if (pathname === '/api') {
+      res.end(data);
+  }
+  
+  // NOT FOUND PAGE  
+   else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-server':'HELLO WORLD'
+    });
+    res.end("<h1>Page is not found!</h1>");
+  }
+});
+
+server.listen('8000', '127.0.0.1', () => {
+  console.log("Listening request on port 8000");
+})
